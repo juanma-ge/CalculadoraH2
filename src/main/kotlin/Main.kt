278b107c971785ba433a402c7f27acdc204b0949
juanma-ge <jgomest545@g.educaand.es>
2025-05-23 00:15:36 +0200

    corección de carpetas

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,36 @@
 package org.example
 
-fun main(){
+import es.prog2425.calclog.app.Controlador
+import es.prog2425.calclog.data.DatabaseConfig.initDatabase
+import es.prog2425.calclog.data.RepoLogTxt
+import es.prog2425.calclog.service.ServicioCalc
+import es.prog2425.calclog.service.ServicioLog
+import es.prog2425.calclog.ui.Consola
+import es.prog2425.calclog.utils.GestorFichTxt
 
+/**
+ * Punto de entrada de la aplicación.
+ *
+ * Inicializa los componentes necesarios de la arquitectura (UI, repositorio, servicio, lógica de negocio)
+ * y delega el control al controlador principal de la aplicación.
+ */
+fun main(args: Array<String>) {
+
+    val repoLog = RepoLogTxt(GestorFichTxt())
+    Controlador(Consola(), ServicioCalc(), ServicioLog(repoLog)).iniciar(args)
+
+    initDatabase()
+
+    /*
+    O también instanciando en variables locales... es lo mismo al fin y al cabo.
+
+    val consola = Consola()
+    val gestorFicheros = GestorFichText()
+    val repoLog = RepoLogTxt(gestorFicheros)
+    val servicioLog = ServicioLog(repoLog)
+    val calculadora = ServicioCalc()
+    val controlador = Controlador(consola, calculadora, servicioLog)
+
+    controlador.iniciar(args)
+    */
 }
